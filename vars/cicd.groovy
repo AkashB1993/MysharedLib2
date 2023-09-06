0ef3402f5981bc55b180a2247dddaab1f0529c6f
AkashB1993 <142805900+AkashB1993@users.noreply.github.com>
2023-09-06 16:09:45 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,4 +10,9 @@ def newDeployment(jobname,ip,appname)
 {
   sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
+def runselenium(jobname)
+{
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/jenkins.war"
+}
+
 
